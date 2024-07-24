b8a3bde936940299b2f622ea9af0688232b0b886
Ahmadxon Eshonbuvaxonov <ahmadhon.eshonbuvahonov@yandex.com>
2024-07-24 07:16:49 +0500

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,73 +1,71 @@
-import { Carusel, CaruselCatalog, CaruselProduct, Useful, Adventages, About } from "@/components/ui";
+import { Carusel , CaruselCatalog ,  CaruselProduct, Useful, Adventages, About} from "@/components/ui";
 import { Card1 } from "@/components/ui/card";
 
 export default function HomePage() {
   return (
     <main className="">
-      <section className="pt-5 pb-[83px] bg-[#F2F2F2]">
-        <div className="container mx-auto px-2.5">
-          <div className="flex items-start gap-[24px]">
-            <div className="min-w-[819px] h-[488px]">
-              <Carusel />
+      <section  className=" pt-5 pb-[83px] bg-[#F2F2F2]">
+        <div className=" container mx-auto px-2.5">
+            <div className="flex items-start gap-[24px]">
+              <div className=" min-w-[819px] h-[488px]">
+                <Carusel />
+              </div>
+              <div className="">
+                  <Card1/> 
+              </div>
+
             </div>
-            <div className="">
-              <Card1 />
-            </div>
-          </div>
         </div>
       </section>
-
       <section>
-        <div className="container mx-auto px-2.5">
+        <div className="container mx-auto px-2 5">
           <div className="py-[24px]">
-            <h1 className="text-[32px] mb-[31px]">Katalog</h1>
+            <h1 className="text-[32px] mb-[31px]">Катаолог</h1>
             <div>
               <CaruselCatalog />
             </div>
           </div>
         </div>
       </section>
-
-      <section className="bg-[#F2F2F2] pb-[80px]">
-        <div className="container mx-auto px-2.5">
+      <section className="bg-[#F2F2F2] pb-[80px] ">
+        <div className="container mx-auto px-2 5">
           <div className="pt-[76px]">
-            <h1 className="text-[32px] mb-[31px]">Aksiya</h1>
+            <h1 className="text-[32px] mb-[31px]">Акция</h1>
             <div>
-              <CaruselProduct text="Aksiya" bg="#FF1C1C" />
+              <CaruselProduct text="Акция" bg="#FF1C1C"/>
             </div>
           </div>
           <div className="pt-[76px]">
-            <h1 className="text-[32px] mb-[31px]">Yangi mahsulotlar</h1>
+            <h1 className="text-[32px] mb-[31px]">Новинки</h1>
             <div>
-              <CaruselProduct text="Yangi" bg="#917BFF" />
+              <CaruselProduct text="Новый" bg="#917BFF"/>
             </div>
           </div>
           <div className="pt-[76px]">
-            <h1 className="text-[32px] mb-[31px]">Mahsulotlar</h1>
+            <h1 className="text-[32px] mb-[31px]">Продукты</h1>
             <div>
               <CaruselProduct />
             </div>
           </div>
           <div className="pt-[76px]">
-            <h1 className="text-[32px] mb-[31px]">Eng ko'p sotilgan</h1>
+            <h1 className="text-[32px] mb-[31px]">ТОП продажа</h1>
             <div>
-              <CaruselProduct text="TOP" bg="#10DE24" />
+              <CaruselProduct text="ТОП" bg="#10DE24" />
             </div>
           </div>
-
           <div className="pt-[76px]">
             <div>
-              <Useful bg="#10DE24" />
+              <Useful  bg="#10DE24" />
             </div>
           </div>
           <div className="pt-[36px]">
             <div>
-              <Adventages bg="#10DE24" />
+              <Adventages  bg="#10DE24" />
             </div>
           </div>
           <div className="pt-[36px]">
             <div>
-              <About bg="#10DE24" />
+              <About  bg="#10DE24" />
             </div>
           </div>
         </div>
